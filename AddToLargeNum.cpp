30ea54535b44ff04ecb8be0799344e309d6859f9
suliutree <413474315@qq.com>
2015-08-18 21:22:54 +0800

    大数相加，正负数相加均支持

--- AddToLargeNum.cpp
+++ AddToLargeNum.cpp
@@ -1,15 +1,15 @@
-/************************************************************************/
-/* 大数相加，目前只支持两个数同为正数或同为负数                         */
-/************************************************************************/
+//************************************************************************/
+//* 大数相加，正负数相加均支					                         */
+//************************************************************************/
 
 #include <iostream>
 #include <stack>
 
 using namespace std;
 
-stack<int>s1;
-stack<int>s2;
-stack<int>s3;
+stack<int> s1;
+stack<int> s2;
+stack<int> s3;
 
 void two_positive(const char* num1, const char* num2, char* result)
 {
@@ -109,6 +109,103 @@ void two_negative(const char* num1, const char* num2, char* result)
 	*result = '\0';
 }
 
+int my_strcmp(const char* num1, const char* num2)
+{
+	int len1 = strlen(num1);
+	int len2 = strlen(num2);
+
+	if (len1 == len2)
+		return strcmp(num1, num2);
+	else if (len1 > len2)
+		return 1;
+	else
+		return -1;
+}
+
+
+void sub(const char* num1, const char* num2, char* result)
+{
+	int len1 = strlen(num1);
+	int len2 = strlen(num2);
+
+	for (int i = 0; i < len1; ++i)
+		s1.push(num1[i] - '0');
+
+	// 这里num2第一位是负号
+	for (int i = 1; i < len2; ++i)
+		s2.push(num2[i] - '0');
+
+	int cmp = my_strcmp(num1, num2+1);
+
+	// 两数除符号位外相等
+	if (cmp == 0)
+	{
+		*result = '0';
+		*result++ = '\0';
+		return;
+	}
+
+	int tmp = 0;
+	while (!s1.empty() && !s2.empty())
+	{
+		if (cmp > 0)
+		{
+			tmp += s1.top() - s2.top();
+			s1.pop();
+			s2.pop();
+			if (tmp < 0) 
+			{
+				int prev = s1.top() - 1; //借位
+				s1.pop();
+				s1.push(prev);
+				tmp += 10;
+			}
+			s3.push(tmp);
+			tmp = tmp / 10;
+		}
+		else if (cmp < 0)
+		{
+			tmp += s2.top() - s1.top();
+			s1.pop();
+			s2.pop();
+			if (tmp < 0) 
+			{
+				int prev = s2.top() - 1; //借位
+				s2.pop();
+				s2.push(prev);
+				tmp += 10;
+			}
+			s3.push(tmp);
+			tmp = tmp / 10;
+		}
+	}
+
+	while (!s1.empty())
+	{
+		s3.push(s1.top());
+		s1.pop();
+	}
+
+	while (!s2.empty())
+	{
+		s3.push(s2.top());
+		s2.pop();
+	}
+
+	// 两数相减之后高位有可能为0，例如222和-221
+	while (s3.top() == 0)
+		s3.pop();
+	
+	if (cmp < 0)
+		*result++ = '-';
+	while (!s3.empty())
+	{
+		*result++ = s3.top() + '0';
+		s3.pop();
+	}
+	*result = '\0';
+}
+
 
 void add (const char *num1, const char *num2, char *result)
 {
@@ -120,8 +217,15 @@ void add (const char *num1, const char *num2, char *result)
 	{
 		two_negative(num1, num2, result);
 	}
+	else if (num1[0] == '-' && num2[0] != '-')
+	{
+		sub(num2, num1, result);
+	}
+	else if (num1[0] != '-' && num2[0] == '-')
+	{
+		sub(num1, num2, result);
+	}
 }
-
 
 int main(void)
 {
